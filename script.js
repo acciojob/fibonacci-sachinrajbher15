2766896623ff1c59db699b5ea346ea33a1060de3
SACHIN KUMAR RAJBHER <86919694+sachinrajbher15@users.noreply.github.com>
2024-09-22 20:07:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
-// your code here
+	// your code here
+	if(num === 1) return 0;
+	if(num === 2) return 1;
+
+	let a = 0;
+	let b = 1;
+	let ans;
+
+	for(let i=3; i<=num; i++){
+		ans = a + b;
+		a = b;
+		b = ans;
+	}
+	return b;
 }
 
 module.exports = fibonacci;
